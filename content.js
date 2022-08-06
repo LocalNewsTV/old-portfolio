b3159bc9805212cf59e8aa76c7bbb1edd10469d7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2022-08-05 19:13:24 -0700

    committing current work

---
+++ content.js
@@ -0,0 +1,8 @@
+projects = [
+    {
+        id: "blackjack",
+        title: "BlackJack",
+        sourceImage: "http://www.fillmurray.com/300/300",
+        description: "Must go faster... go, go, go, go, go! I was part of something special. I was part of something special. Yes, Yes, without the oops! Do you have any idea how long it takes those cups to decompose. Hey, take a look at the earthlings. Goodbye! They're using our own satellites against us. And the clock is ticking."
+    }
+]
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+newE = (type) => document.createElement(type);
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,3 +8,41 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const projectCardMaker = (project) => {
+    const cardMain = newE('div');
+    $(cardMain).addClass('projectCard');
+
+    const card = newE('div');
+    card.id = project.id;
+    $(card).addClass('content');
+    
+    const imageContainer = newE('div');
+    const cardImage = newE('img');
+    cardImage.src = project.sourceImage;
+    $(imageContainer).append(cardImage);
+    $(card).append(imageContainer)
+
+    const cardBody = newE('div');
+    $(cardBody).addClass('contentText');
+    $(card).append(cardBody);
+
+    const cardTitle = newE('h2');
+    $(cardTitle).html(project.title);
+    $(cardBody).append(cardTitle);
+
+    const cardText = newE('p');
+    $(cardText).html(project.description);
+    $(cardBody).append(cardText);
+
+    const button = newE('button');
+    $(button).attr("data-toggle", "modal");
+    $(button).attr("data-target", "projectModal");
+    // $(button).on('click', showProject.bind(this, project));
+    $(button).html("More");
+    $(cardBody).append(button);
+    $(cardMain).append(card);
+    return cardMain;
+}
+
+$(document).ready(()=>{$('#projectsBody').append(projectCardMaker(projects[0]));$('#projectsBody').append(projectCardMaker(projects[0]));$('#projectsBody').append(projectCardMaker(projects[0]));$('#projectsBody').append(projectCardMaker(projects[0]));$('#projectsBody').append(projectCardMaker(projects[0]));})
